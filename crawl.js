dbf082631dd98045c15c6acae4d9056b74ceee2a
Jose Angel Vergara <joseaveridge@hotmail.com>
2023-03-11 10:09:15 -0500

    geturltoHTML tested

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsfromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log("error with relative URL, ", error.message);
+      }
+    } else {
+      //absolute
+      try {
+        const urlObj = new URL(`${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log("error with absolute URL, ", error.message);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +37,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsfromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsfromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL", () => {
@@ -26,5 +26,79 @@ test("normalizeURL strip http", () => {
   const input = "http://BLOG.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsfromTML absolute", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">
+            Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+
+  const inputBaseURL = "https://blog.boot.dev/path/";
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsfromTML relative", () => {
+  const inputHTMLBody = `
+      <html>
+          <body>
+              <a href="/path">
+              Boot.dev Blog
+              </a>
+          </body>
+      </html>
+      `;
+
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/path"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsfromTML Both", () => {
+  const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog path one
+                </a>
+                <a href="/path2/">
+                Boot.dev Blog path two
+                </a>
+            </body>
+        </html>
+        `;
+
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://blog.boot.dev/path1/",
+    "https://blog.boot.dev/path2/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsfromTML invalid", () => {
+  const inputHTMLBody = `
+      <html>
+          <body>
+              <a href="invalid">
+              Invalid URL
+              </a>
+          </body>
+      </html>
+      `;
+
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsfromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
